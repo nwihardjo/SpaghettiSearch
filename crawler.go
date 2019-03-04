e36269498770a16262f6a52748846fece4a5bdb2
Petra Gabriela <pgabriela@connect.ust.hk>
2019-03-04 13:54:55 +0800

    Used threading and queue for crawling

--- crawler.go
+++ crawler.go
@@ -1,99 +1,128 @@
 package main
 
-
 import (
+	"crypto/tls"
 	"fmt"
+	"github.com/eapache/channels"
+	"golang.org/x/net/html"
 	"net/http"
 	"os"
-	"golang.org/x/net/html"
-	"github.com/eapache/channels"
-	"crypto/tls"
+	"strconv"
+	"sync"
+	"time"
 )
 
 func main() {
 	fmt.Println("Crawler started...")
 
+	start := time.Now()
+
 	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify : true},
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
 	client := &http.Client{Transport: tr}
 
 	startURL := "https://www.cse.ust.hk/"
-	numOfPages := 10
-	visited := make(chan string, numOfPages)
-	queue := channels.NewResizableChannel()
+	numOfPages := 30
+	maxThreadNum := 10
+	visited := channels.NewInfiniteChannel()
+	queue := channels.NewInfiniteChannel()
 
 	queue.In() <- startURL
 
-	var f func(*html.Node)
-	f = func(n *html.Node) {
+	var f func(*html.Node, string)
+	f = func(n *html.Node, baseURL string) {
 		if n.Type == html.ElementNode && n.Data == "a" {
 			for a := 0; a < len(n.Attr); a++ {
 				if n.Attr[a].Key == "href" {
-					if n.Attr[a].Val == "" || string(n.Attr[a].Val[0]) == "#" {
+					if n.Attr[a].Val == "" || n.Attr[a].Val[0] == '#' {
 						continue
 					}
-					if string(n.Attr[a].Val[0]) == "/" {
-						queue.Resize(channels.BufferCap(queue.Len() + 1))
-						queue.In() <- "https://www.cse.ust.hk" + string(n.Attr[a].Val)
+					if n.Attr[a].Val[0] == '/' {
+						if baseURL[len(baseURL)-1] == '/' {
+							queue.In() <- baseURL[:len(baseURL)-1] + n.Attr[a].Val
+						} else {
+							queue.In() <- baseURL + n.Attr[a].Val
+						}
 					} else {
-						queue.Resize(channels.BufferCap(queue.Len() + 1))
-						queue.In() <- string(n.Attr[a].Val)
+						queue.In() <- n.Attr[a].Val
 					}
+					break
 				}
 			}
 		}
 		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			f(c)
+			f(c, baseURL)
 		}
 	}
 
-	for ; len(visited) < numOfPages && queue.Len() > 0; {
-		if currentURL, ok := (<-queue.Out()).(string); ok {
-			flag := false
-			var temp []string
-			close(visited)
-			for v := range visited {
-				temp = append(temp, v)
-				if(v == currentURL) {
-					flag = true
-				}
-			}
-			visited = make(chan string, numOfPages)
-			for _, t := range temp {
-				visited <- t
-			}
-			if flag {
-				continue
-			}
-			fmt.Println("Visiting " + currentURL)
-			resp, err := client.Get(currentURL)
-			visited <- currentURL
+	var crawl func(int, *sync.WaitGroup, string)
+	crawl = func(idx int, wg *sync.WaitGroup, currentURL string) {
+		defer wg.Done()
 
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
+		innerStart := time.Now()
+		resp, err := client.Get(currentURL)
+		fmt.Println("Goroutine id " + strconv.Itoa(idx) + " visited " + currentURL + " (elapsed time: " + time.Now().Sub(innerStart).String() + ")")
 
-			doc, err := html.Parse(resp.Body)
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-
-			f(doc)
-
-			resp.Body.Close()
-		} else {
+		if err != nil {
+			fmt.Println(err)
 			os.Exit(1)
 		}
+
+		doc, err := html.Parse(resp.Body)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		f(doc, currentURL)
+
+		resp.Body.Close()
 	}
 
-	close(visited)
+	var wg sync.WaitGroup
 
-	for v := range visited {
-		fmt.Println(v)
+	for visited.Len() < numOfPages {
+		for idx := 0; queue.Len() > 0 && idx < maxThreadNum && visited.Len() < numOfPages; idx++ {
+			if currentURL, ok := (<-queue.Out()).(string); ok {
+				flag := false
+				var temp []string
+				for i := 0; i < visited.Len(); i++ {
+					v, ok2 := (<-visited.Out()).(string)
+					if !ok2 {
+						break
+					}
+					temp = append(temp, v)
+					if v == currentURL {
+						flag = true
+					}
+				}
+				for _, t := range temp {
+					visited.In() <- t
+				}
+				if flag {
+					idx--
+					continue
+				}
+				visited.In() <- currentURL
+
+				wg.Add(1)
+				go crawl(idx, &wg, currentURL)
+
+			} else {
+				os.Exit(1)
+			}
+		}
+
+		wg.Wait()
+
+		if queue.Len() <= 0 {
+			break
+		}
 	}
 
+	visited.Close()
 	queue.Close()
+
+	fmt.Println("\nTotal elapsed time: " + time.Now().Sub(start).String())
 }
