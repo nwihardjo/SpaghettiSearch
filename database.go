b6adbb8e21dcd606e89370fd6f43b1c4ad1ddeb5
Nathaniel Wihardjo <nwihardjo@connect.ust.hk>
2019-03-22 11:23:07 +0800

    defined db interface

--- database.go
+++ database.go
@@ -1,14 +1,63 @@
 package main
 
 import (
+//	"context"
+//	"os"
+//	"time"
 	"fmt"
 	"log"
 	"github.com/dgraph-io/badger"
 )
 
+const (
+	// Default values are used. For garbage-collection purposes
+	// TODO: to be fine-tuned
+	badgerDiscardRatio = 0.5
+	badgerGCInterval = 10 * time.Minute
+)
+
+
+var (
+	// BadgerAlertNamespace defines the alerts BadgerDB namespace
+	BadgerAlertNamespace = []byte("alerts")
+)
+
+type (
+	// TODO: investigate namespace necessity
+	DB interface {
+		Get(namespace, key []byte) (value []byte, err error)
+		Set(namespace, key, value []byte) error
+		Has(namespace, key []byte) (bool, error)
+		Delete(namespace, key []byte) (bool, error)
+		Close() error
+	}
+
+	BadgerDB struct {
+		db	*badger.DB
+		logger 	Logger
+	}
+)
+
+func NewBadgerDB(dir string, logger log.Logger)(DB, error){
+	// create directory with specified permission, although badger supports this automatically
+	if err := os.MkdirAll(dataDir, 0774); err != nil {
+		return nil, err
+	}
+
+	opts := badger.DefaultOptions
+	// set SyncWrites to False for performance increase but may cause loss of data
+	opts.SyncWrites = true
+	opts.Dir, opts.ValueDir = dataDir, dataDir
+	
+	bdb := &BadgerDB {
+		db:	badgerDB,
+		logger:	logger.With("module", "db"),
+	}
+	
+
 func main() {
 	// testing db
-	tmpDir := "/home/asus/go/tmp/badger/"
+	tmpDir := "./database/"
 	
 	opts:= badger.DefaultOptions
 	opts.Dir = tmpDir
