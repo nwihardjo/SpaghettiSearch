ee396ee4d94195a74def964bef083d4b8c6342a4
Petra Gabriela <pgabriela@connect.ust.hk>
2019-03-27 11:31:40 +0800

    Added file save in indexer, added new waitgroup in start_crawl for indexer sync, integrated crawler with indexer

--- indexer/indexer.go
+++ indexer/indexer.go
@@ -2,8 +2,27 @@ package indexer
 
 import (
 	"fmt"
+	//"encoding/json"
+	"time"
+	"io/ioutil"
+	"sync"
+	"strings"
+	"os"
 )
 
-func Index(doc []byte, url string, lastModified string) {
-	fmt.Println("Indexing")
+func Index(doc []byte, url string, lastModified time.Time, wgIndexer *sync.WaitGroup) {
+	defer wgIndexer.Done()
+
+	// fmt.Println("Indexing")
+
+	// Get Last Modified from DB
+
+	// Save to file
+	if _, err := os.Stat("docs/"); os.IsNotExist(err) {
+		os.Mkdir("docs/", 0755)
+	}
+	err := ioutil.WriteFile("docs/" + strings.ReplaceAll(strings.ReplaceAll(url, "https://", ""), "/", "_"), doc, 0644)
+	if err != nil {
+		panic(err)
+	}
 }
