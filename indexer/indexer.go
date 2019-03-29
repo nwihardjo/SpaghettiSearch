ae7898571d9e3a0e0210ad3cbc9051d6a1f3f20b
ak2411 <akosasih@connect.ust.hk>
2019-03-29 18:14:13 +0800

    parsing test

--- indexer/indexer.go
+++ indexer/indexer.go
@@ -1,80 +1,110 @@
-package indexer
+package main
 
 import (
-	"../database"
-	"context"
-	//"fmt"
-	"strconv"
+	// "../database"
+	// "context"
+	"fmt"
+	"golang.org/x/net/html"
+	// "reflect"
+	// "strconv"
 	//"github.com/apsdehal/go-logger"
-	"time"
+	// "time"
 	"io/ioutil"
-	"sync"
-//	"strings"
-	"os"
-	"net/url"
-	"github.com/dchest/stemmer/porter2"
+	"bytes"
+	// "sync"
+	"strings"
+	// "os"
+	// "io"
+	// "regexp"
+	// "net/url"
+	// "github.com/dchest/stemmer/porter2"
 )
 
 var docsDir = "docs/"
 
-func Index(doc []byte, urlString string, lastModified time.Time,
-	wgIndexer *sync.WaitGroup, mutex *sync.Mutex,
-	inverted []database.DB_Inverted, forward []database.DB) {
+// func Index(doc []byte, urlString string, lastModified time.Time,
+// 	wgIndexer *sync.WaitGroup, mutex *sync.Mutex,
+// 	inverted []database.DB_Inverted, forward []database.DB) {
+//
+// 	defer wgIndexer.Done()
+//
+// 	ctx, _ := context.WithCancel(context.TODO())
+//
+// 	// fmt.Println("Indexing")
+// 	// Set stemmer
+// 	eng := porter2.Stemmer // sample: eng.Stem("delicious")
+// 	// Get Last Modified from DB
+// 	URL, err := url.Parse(urlString)
+//
+// 	if err != nil {
+// 		panic(err)
+// 	}
+//
+// 	fmt.Println(URL.String())
+//
+// 	//BEGIN LOCK//
+// 	mutex.Lock()
+// 		nextDocIDBytes, errNext := forward[4].Get(ctx, []byte("nextDocID"))
+// 		if errNext != nil {
+// 			panic(errNext)
+// 		}
+//
+// 		nextDocID, err := strconv.Atoi(string(nextDocIDBytes))
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		forward[4].Set(ctx, []byte("nextDocID"), []byte(strconv.Itoa(nextDocID + 1)))
+// 	mutex.Unlock()
+// 	//END LOCK//
+//
+// 		// Save to file
+// 		if _, err := os.Stat(docsDir); os.IsNotExist(err) {
+// 			os.Mkdir(docsDir, 0755)
+// 		}
+// 		err = ioutil.WriteFile(docsDir + strconv.Itoa(nextDocID), doc, 0644)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		URLBytes, errMarshal := URL.MarshalBinary()
+// 		if errMarshal != nil {
+// 			panic(errMarshal)
+// 		}
+// 		forward[4].Set(ctx, []byte(strconv.Itoa(nextDocID)), URLBytes)
+// }
 
-	defer wgIndexer.Done()
-
-	ctx, _ := context.WithCancel(context.TODO())
-
-	// fmt.Println("Indexing")
-
-	// Regular keyword-based indexes
-	// Mapping indexes
-		// Word to wordID, url to pageID
-	// Inverted file indexes
-		// wordID to {pageID, <word positions>}
-	// Forward index
-		// pageID to {keywords}
-	// Page properties
-		// pageID to title, URL, size, last date of mod, etc
-	// Link based indexes
-		// Child pageID to parent pageID
-
-		// Get Last Modified from DB
-		URL, err := url.Parse(urlString)
-		if err != nil {
-			panic(err)
-		}
-	// Set stemmer
-	eng := porter2.Stemmer // sample: eng.Stem("delicious")
+func main() {
+	var title string
+	var prevToken string
+	var words []string
+	var cleaned string
+	sample, err := ioutil.ReadFile("./Department of Computer Science and Engineering - HKUST.html")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(URL.String())
-
-	mutex.Lock()
-	nextDocIDBytes, errNext := forward[4].Get(ctx, []byte("nextDocID"))
-	if errNext != nil {
-		panic(errNext)
+	//Tokenize document
+	tokenizer := html.NewTokenizer(bytes.NewReader(sample))
+	for {
+		tokenType := tokenizer.Next()
+		// end of file or html error
+		if tokenType == html.ErrorToken {
+			break
+		}
+		token := tokenizer.Token()
+		switch tokenType {
+		case html.StartTagToken:
+			if token.Data == "title" {
+				tokenizer.Next()
+				title = strings.TrimSpace(tokenizer.Token().Data)
+			}
+			prevToken = token.Data
+			break
+		case html.TextToken:
+			cleaned = strings.TrimSpace(token.Data)
+			if prevToken != "script" && prevToken != "style" && cleaned != "" {
+				words = append(words, cleaned)
+			}
+		}
 	}
-
-	nextDocID, err := strconv.Atoi(string(nextDocIDBytes))
-	if err != nil {
-		panic(err)
-	}
-	forward[4].Set(ctx, []byte("nextDocID"), []byte(strconv.Itoa(nextDocID + 1)))
-	mutex.Unlock()
-
-		// Save to file
-		if _, err := os.Stat(docsDir); os.IsNotExist(err) {
-			os.Mkdir(docsDir, 0755)
-		}
-		err = ioutil.WriteFile(docsDir + strconv.Itoa(nextDocID), doc, 0644)
-		if err != nil {
-			panic(err)
-		}
-		URLBytes, errMarshal := URL.MarshalBinary()
-		if errMarshal != nil {
-			panic(errMarshal)
-		}
-		forward[4].Set(ctx, []byte(strconv.Itoa(nextDocID)), URLBytes)
+	fmt.Println(words[4])
+	fmt.Println(title)
 }
