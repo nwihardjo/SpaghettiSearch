eaee399f5a70ad61e8f2d99404686b7cbd4199fd
Nathaniel Wihardjo <nwihardjo@connect.ust.hk>
2019-06-20 17:47:16 +0800

    update

---
+++ interface/build/precache-manifest.3e910c4c0201110797944cd8ecd75fa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c7b96809fc8ba6cb89fb",
+    "url": "/static/js/main.f5cc8506.chunk.js"
+  },
+  {
+    "revision": "bd7d79f7f347c64079db",
+    "url": "/static/js/2.eb785a58.chunk.js"
+  },
+  {
+    "revision": "c7b96809fc8ba6cb89fb",
+    "url": "/static/css/main.47288510.chunk.css"
+  },
+  {
+    "revision": "bd7d79f7f347c64079db",
+    "url": "/static/css/2.c49b2b87.chunk.css"
+  },
+  {
+    "revision": "a08ce9a66afb26e301dc521fa7973467",
+    "url": "/index.html"
+  }
+];
