b40ce6d5da084d010629e72d536ede09c7671c9d
Nathaniel Wihardjo <nwihardjo@connect.ust.hk>
2019-06-20 17:44:26 +0800

    update

--- interface/src/config/server.js
+++ interface/src/config/server.js
@@ -1,5 +1,5 @@
 const PORT = process.env.PORT || 8080;
 const HOST = '0.0.0.0' || 'localhost';
-let address = "http://0.0.0.0:" + PORT + "/";
+let address = "http://localhost:" + PORT + "/";
 
 module.exports = {address: address};
